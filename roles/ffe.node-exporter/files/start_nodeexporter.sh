d0b9db4e6a280374f38278d127f73d1ec1fc1ff5
Daniel Czerwonk <daniel@dan-nrw.de>
2016-03-27 21:38:34 +0200

    node-exporter does not run in docker any more

--- roles/ffe.node-exporter/files/start_nodeexporter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker rm node-exporter
-docker run -d -p 9100:9100 -v "/proc:/host/proc" -v "/sys:/host/sys" -v "/:/rootfs" --net="host" --name node-exporter prom/node-exporter -collector.procfs /host/proc -collector.sysfs /host/proc -collector.filesystem.ignored-mount-points "^/(sys|proc|dev|host|etc)($|/)"
